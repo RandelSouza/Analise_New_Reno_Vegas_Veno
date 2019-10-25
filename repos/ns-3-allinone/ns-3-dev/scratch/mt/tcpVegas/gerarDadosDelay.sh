2fe812e6e34e71eeb6c1521e53619fe5aeec3c10
RandelSouza <randelsouza88@gmail.com>
2019-10-25 15:46:46 -0300

    "code modified to get more organized manhattan vegas update "

--- repos/ns-3-allinone/ns-3-dev/scratch/mt/tcpVegas/gerarDadosDelay.sh
+++ repos/ns-3-allinone/ns-3-dev/scratch/mt/tcpVegas/gerarDadosDelay.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Protocolo TCP utilizado 1(New Reno)
-typeTCP=(2)
+# Protocolo TCP utilizado  2 - Vegas
+typeTCP=( 2 )
+
 # Taxa de erro utilizada
-errorRate=(0.2 0.5)
+errorRate=( 0.2 0.5 )
+
 # Número de nós presente na topologia de rede manhattan
-#numberNodes=(9 36 64)
-numberNodes=(3 6 8)
-# Taxa de trasmissão in Mbps
-dataRate=(10 100)
+numberNodes=( 9 36 64 )
+
+# Taxa de trasmissão em Mbps
+dataRate=( 10 100 )
 
 NS_LOG=manhathan=info
 
@@ -22,13 +24,11 @@ do
 	do
 	    for rate in "${dataRate[@]}"
 	    do
-	    	echo $tcp, $error, $node, $rate
-		nodesQuantity=$node;			
-		
-		cat log_manhattan-$tcp-$error-$nodesQuantity-$rate.txt  | grep delay | cut -d":" -f2 | cut -d" " -f4 > Delay/Delay_manhattan-$tcp-$error-$nodesQuantity-$rate.txt
+	    	echo $tcp, $error, $node, $rate;
+		nodesQuantity=$node;
 
+		cat log_manhattan-$tcp-$error-$nodesQuantity-$rate.txt  | grep delay | cut -d":" -f2 | cut -d" " -f4 > Delay/Delay_manhattan-$tcp-$error-$nodesQuantity-$rate.txt;
 	    done
 	done
     done
 done
-
