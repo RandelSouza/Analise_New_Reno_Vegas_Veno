0b2b9b3abbbd28d9a716d784caec6ad2bfcf6d4e
RandelSouza <randelsouza88@gmail.com>
2019-10-25 12:57:29 -0300

    "code modified to get more organized 2"

--- repos/ns-3-allinone/ns-3-dev/scratch/halteres/halteres.sh
+++ repos/ns-3-allinone/ns-3-dev/scratch/halteres/halteres.sh
@@ -2,13 +2,15 @@
 
 # Protocolo TCP utilizado 1(New Reno), 2(Vegas) e 3(Veno)
 typeTCP=(1 2 3)
+
 # Taxa de erro utilizada
 errorRate=(0.2 0.5)
+
 # Número de nós presente na topologia de rede halteres
 numberNodes=(9 36 64)
+
 # Taxa de trasmissão in Mbps
 dataRate=(10 100)
-
 NS_LOG=halteres=info
 
 for tcp in "${typeTCP[@]}"
@@ -19,19 +21,19 @@ do
     	do
     	    for rate in "${dataRate[@]}"
     	    do
-        	    echo $tcp, $error, $node, $rate
-        		nodesQuantity=$[$node * $node];
-        		nodesQuantity=$[$nodesQuantity / 2]
+        	echo $tcp, $error, $node, $rate;
+                nodesQuantity=$[$node * $node];
+        	nodesQuantity=$[$nodesQuantity / 2];
 
-        		./../../waf --run "halteres --typeTCP="$tcp" --errorRate="$error" --nLeftLeaf="$nodesQuantity" --nRightLeaf="$nodesQuantity" --dataRate="$rate"";
+        	./../../waf --run "halteres --typeTCP="$tcp" --errorRate="$error" --nLeftLeaf="$nodesQuantity" --nRightLeaf="$nodesQuantity" --dataRate="$rate"";
 
-        		mkdir -p halteres-$tcp-$error-$nodesQuantity-$rate;
-        		mkdir -p halteres-$tcp-$error-$nodesQuantity-$rate-xml;
-        		mkdir -p halteres-$tcp-$error-$nodesQuantity-$rate-tr;
+        	mkdir -p halteres-$tcp-$error-$nodesQuantity-$rate;
+        	mkdir -p halteres-$tcp-$error-$nodesQuantity-$rate-xml;
+        	mkdir -p halteres-$tcp-$error-$nodesQuantity-$rate-tr;
 
-        		mv ../../*.pcap halteres-$tcp-$error-$nodesQuantity-$rate;
-        		mv ../../*.tr   halteres-$tcp-$error-$nodesQuantity-$rate-tr;
-        		mv ../../*.xml  halteres-$tcp-$error-$nodesQuantity-$rate-xml;
+        	mv ../../*.pcap halteres-$tcp-$error-$nodesQuantity-$rate;
+        	mv ../../*.tr   halteres-$tcp-$error-$nodesQuantity-$rate-tr;
+        	mv ../../*.xml  halteres-$tcp-$error-$nodesQuantity-$rate-xml;
     	    done
     	done
     done
