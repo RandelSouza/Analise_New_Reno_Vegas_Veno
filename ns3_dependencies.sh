87658b0e49e22a1e47a9347c1f6ca39552865051
RandelSouza <randelsouza88@gmail.com>
2019-10-27 19:15:31 -0300

    add file ns3_dependencies.sh

---
+++ ns3_dependencies.sh
@@ -0,0 +1,13 @@
+			<textarea id="text">sudo apt-get install python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev ipython -y ;
+sudo apt-get install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev -y  ;
+sudo apt-get install autoconf cvs bzr unrar -y ;
+sudo apt-get install gdb valgrind -y ;
+sudo apt-get install doxygen graphviz imagemagick -y ;
+sudo apt-get install texlive texlive-extra-utils texlive-latex-extra texlive-font-utils texlive-lang-portuguese dvipng latexmk -y ;
+sudo apt-get install gsl-bin libgsl-dev libgsl23 libgslcblas0 -y ;
+sudo apt-get install tcpdump -y ;
+sudo apt-get install sqlite sqlite3 libsqlite3-dev -y ;
+sudo apt-get install libxml2 libxml2-dev -y ;
+sudo apt-get install libgtk2.0-0 libgtk2.0-dev -y ;
+sudo apt-get install vtun lxc -y ;
+sudo apt-get install libboost-signals-dev libboost-filesystem-dev -y ;
